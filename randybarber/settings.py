e938e5e42b69e59b1baa2401404e22aba6316d46
Benohene <>
2023-06-19 10:16:26 +0000

    Deployment commit

--- randybarber/settings.py
+++ randybarber/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-benohene-pp4-randy-barbe-yp9v9459va.us2.codeanyapp.com", "randy-barber-app.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["8000-benohene-pp4-randy-barbe-yp9v9459va.us2.codeanyapp.com", "randy-barber-app-bd2e48941993.herokuapp.com", "localhost"]
 
 
 # Application definition
