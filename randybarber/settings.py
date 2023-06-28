0a06044b897e58623abedd7e5322edac5e75934e
Benohene <>
2023-06-28 21:54:15 +0000

    dynamically set debug

--- randybarber/settings.py
+++ randybarber/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ["8000-benohene-pp4-randy-barbe-yp9v9459va.us2.codeanyapp.com", "randy-barber-app.herokuapp.com", "localhost", 'randy-barber-app-bd2e48941993.herokuapp.com']
 
